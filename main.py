324c372d6ef626e236e5df901397e2a90d6f4694
Akshaykumar Salunke <cruseakshay@gmail.com>
2023-07-30 11:57:54 +0000

    url change

--- main.py
+++ main.py
@@ -12,7 +12,8 @@ st.title('DataBricks SQL Connection')
 """
 Ensure Connection configuration - Secrets. [Refer.](https://docs.streamlit.io/streamlit-community-cloud/get-started/deploy-an-app/connect-to-data-sources/secrets-management)
 """
-st.image("https://github.com/cruseakshay/DeltaConnector/blob/2f4e050e71e3b5057cd281220974fa51d97a9762/streamlit_secrets.png")
+htp="https://raw.githubusercontent.com/cruseakshay/DeltaConnector/main/streamlit_secrets.png"
+st.image(htp, caption= 'configuration', width=350)
 
 with st.echo():
     conn = st.experimental_connection('db_sql', type=DeltaConnection)
